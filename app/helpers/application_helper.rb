86f76ed4d75ec7790bfad0d0acf2c1aa9e2a41c1
-_- Zaba -_- <bsilva@korewa.com.br>
2011-08-09 16:50:34 -0300

    Ajustes codigo de barras email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,53 @@
 module ApplicationHelper
+  def traduz_status_do_pedido(status)
+    
+    retorno = ""
+    case status
+      when "completed"
+        retorno = "Completo"
+      
+       when "incompleted"
+          retorno = "Incompleto"
+    
+      when "pending"
+        retorno = "Aguardando Pagamento"
+        
+      when "approved"
+        retorno = "Aprovado"
+        
+      when "verifying"
+        retorno = "Em Análise"
+        
+      when "canceled"
+        retorno = "Cancelado"
+        
+      when "refunded"
+        retorno = "Devolvido"
+    end
+    
+    return retorno
+  end
+  
+  def traduz_tipo_do_pagamento(tipo)
+    retorno = ""
+    case tipo 
+      when "credit_card"
+        retorno = "Cartão de Crédito"
+    
+      when "invoice"
+        retorno = "Boleto"
+        
+      when "pagseguro"
+        retorno = "Transferência entre Pagseguro"
+        
+      when "online_transfer"
+        retorno = "Pagamento Online"
+        
+      when "donation"
+        retorno = "Doação"
+
+    end
+    
+    return retorno
+  end
 end
