b6a780b2ae7731ad2a86751765b2c2489a37199d
-_- Zaba -_- <bsilva@korewa.com.br>
2011-09-02 20:26:32 -0300

    Ajustes de Imagem

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,3 +1,4 @@
 class News < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "385x334>", :thumb => "158x117>" }
+  #has_attached_file :image, :styles => { :medium => "385x334>", :thumb => "158x117>" }
+  has_attached_file :image, :styles => { :medium => "334x385>", :thumb => "117x158" }
 end
