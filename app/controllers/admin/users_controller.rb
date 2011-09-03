5f338e8106d08b43b5a3e220262d3974e7dc08a3
Rafael Matuoka <rmatuoka@korewa.com.br>
2011-09-03 11:11:39 -0300

    Paginacao

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,7 @@ class Admin::UsersController < ApplicationController
   layout "admin"
   
   def index
-    @users = User.all
+    @users = User.all.paginate :page => params[:page],:per_page => 20
   end
 
   def show
@@ -31,7 +31,7 @@ class Admin::UsersController < ApplicationController
   
   def search
     if !params[:keyword].blank?
-      @Results = User.search_for(params[:keyword])
+      @Results = User.search_for(params[:keyword]).paginate :page => params[:page],:per_page => 20
     end
   end
     
