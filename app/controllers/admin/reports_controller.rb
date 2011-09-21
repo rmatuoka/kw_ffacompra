534594a031f378808d0fc7ba4bbbdfa7cf7785cf
-_- Zaba -_- <bsilva@korewa.com.br>
2011-09-21 17:55:14 -0300

    Ajuste SQL

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -4,7 +4,7 @@ class Admin::ReportsController < ApplicationController
     @reports = OrderTicket.find(  :all,
                             :select => "`order_tickets`.`ticket_id`, sum(`order_tickets`.`amount`) as qtde,sum(`order_tickets`.`amount` * `order_tickets`.`price`) as total",
                             :joins => "`order_tickets` INNER JOIN `orders` ON `orders`.`id` = `order_tickets`.`order_id`",
-                            :conditions => ['`orders`.`status` = "completed" or `orders`.`status` = "verifying"'],
+                            :conditions => ['`orders`.`status` = "completed" or `orders`.`status` = "approved"'],
                             :group => '`order_tickets`.`ticket_id`'
                           )
   end
